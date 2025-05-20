d7789840eb21e7aaa2ca7716d44383f23548f911
admodev <admodevcodes@outlook.com>
2025-05-20 20:04:55 -0300

    feat: :sparkles: adds path check for windows install script

--- install.bat
+++ install.bat
@@ -1,8 +1,10 @@
 :: This batch script installs chip software using Jpackage in Windows...
 ECHO OFF
+SET "JAR_PATH=%CD%\app\build\libs"
+
 jpackage ^
   --name chips ^
-  --input app/build/libs ^
+  --input "%JAR_PATH%" ^
   --main-jar app.jar ^
   --type app-image ^
   --icon jpackage/icon.png ^
